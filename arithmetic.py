b8cc3db647fea9a49671946615bbd4faeafb9b67
Chiran071 <chiranaryal224@gmail.com>
2025-12-27 16:48:16 +0545

    first commit

---
+++ arithmetic.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr=np.array([1,2,3,4])
+arr2=np.array([5,6,7,8])
+
+print(arr+arr2)
+print(arr*arr2)
+print(arr/arr2)
+print("mean",np.mean(arr))
+print("standard deviation:",np.std(arr))
+print("variance",np.var(arr))
+
+
